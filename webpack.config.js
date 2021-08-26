f29c61ca4be2a52b2f64d6293558b7b28eed8ee1
tayebehsafdari <66350369+tayebehsafdari@users.noreply.github.com>
2021-08-26 15:27:01 +0430

    modify files

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,6 @@ const webpack = require('webpack');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
-const {extendDefaultPlugins} = require('svgo');
 // const WorkboxPlugin = require('workbox-webpack-plugin');
 
 var config = {
@@ -16,67 +14,6 @@ var config = {
         // hot: true
     },
     stats: 'errors-only',
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'Output Management',
-            filename: 'index.html',
-            template: 'public/index.html',
-            favicon: './public/favicon.ico',
-            meta: {
-                keywords: 'Tayebeh Safdari, Senior Front-End Web Developer, Graphic Designer, UX/UI Designer, Web Designer, HTML, CSS, Bootstrap, Sass, jQuery, Webpack, React, JavaScript',
-                description: 'Senior Front-End Web Developer and UX/UI Designer',
-                author: 'Tayebeh Safdari',
-                viewport: 'width=device-width, initial-scale=1.0, shrink-to-fit=no'
-            }
-        }),
-        new MiniCssExtractPlugin({
-            filename: '[name].css',
-            chunkFilename: '[id].css'
-        }),
-        new webpack.ProvidePlugin({
-            $: 'jquery',
-            jQuery: 'jquery',
-            classie: 'desandro-classie',
-            Filterizr: 'filterizr',
-            Typed: 'typed.js'
-        }),
-        new ImageMinimizerPlugin({
-            minimizerOptions: {
-                plugins: [
-                    ["gifsicle", {interlaced: true}],
-                    ["jpegtran", {progressive: true}],
-                    ["optipng", {optimizationLevel: 5}],
-                    [
-                        "svgo",
-                        {
-                            plugins: extendDefaultPlugins([
-                                {
-                                    name: "removeViewBox",
-                                    active: false
-                                },
-                                {
-                                    name: "addAttributesToSVGElement",
-                                    params: {
-                                        attributes: [{xmlns: "http://www.w3.org/2000/svg"}]
-                                    }
-                                }
-                            ])
-                        }
-                    ],
-                ]
-            }
-        }),
-        /* new WorkboxPlugin.GenerateSW({
-            clientsClaim: true,
-            skipWaiting: true
-        }) */
-    ],
-    output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
-        publicPath: '/'
-    },
     module: {
         rules: [
             {
@@ -125,6 +62,41 @@ var config = {
                 }
             },
         ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Output Management',
+            filename: 'index.html',
+            template: 'public/index.html',
+            favicon: './public/favicon.ico',
+            meta: {
+                keywords: 'Tayebeh Safdari, Senior Front-End Web Developer, Graphic Designer, UX/UI Designer, Web Designer, HTML, CSS, Bootstrap, Sass, jQuery, Webpack, React, JavaScript',
+                description: 'Senior Front-End Web Developer and UX/UI Designer',
+                author: 'Tayebeh Safdari',
+                viewport: 'width=device-width, initial-scale=1.0, shrink-to-fit=no'
+            }
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+            chunkFilename: '[id].css'
+        }),
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery',
+            classie: 'desandro-classie',
+            Filterizr: 'filterizr',
+            Typed: 'typed.js'
+        }),
+        /* new WorkboxPlugin.GenerateSW({
+            clientsClaim: true,
+            skipWaiting: true
+        }) */
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+        publicPath: '/'
     },
     optimization: {
         minimizer: [
